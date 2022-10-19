303b812969b087510e8e81889718af6933072c0a
Mohamed yahya <92155140+Mohamed-Yahyaa@users.noreply.github.com>
2022-10-19 13:06:48 +0100

    Routing controller exemple update

--- The Basics/Routing/routes/web.php
+++ The Basics/Routing/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
+use Illuminate\Http\Request;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -21,6 +24,37 @@ Route::get('/index',function () {
     return view('index');
 });
 
+//Route avec controller
+Route::get('/hello',[TestController::class,'afficher']);
+
+// Methode match 
+
+Route::match(['get','post'],'/page',function(){
+  return('yahya');
+});
+
+// // Methode any
+
+Route::any(('/pages'),function(){
+ return('helle tous le monde');
+});
+
+// //Route request
+Route::any(('/input'),function(){
+    return view('input');
+   });
+
+
+
+//Protection CSRF
+Route::get('/profile',function(){
+    return view('csrf');
+});
+
+Route::any('/csrf',function(request $request){
+    return  $request->input("name");
+    
+ });
 
 
 
