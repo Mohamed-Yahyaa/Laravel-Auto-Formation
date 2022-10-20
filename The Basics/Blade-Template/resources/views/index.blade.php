08265d31bf4f99053563ebe2dac6e00fcbacefbc
Mohamed yahya <92155140+Mohamed-Yahyaa@users.noreply.github.com>
2022-10-20 09:34:06 +0100

    Blade Template exemple update

---
+++ The Basics/Blade-Template/resources/views/index.blade.php
@@ -0,0 +1,30 @@
+@include('title')
+
+{{-- if and else --}}
+
+ id = {{$name=1}}
+ @if ($name == 1)
+    <h2>hello yahya</h2>
+@else
+   <h2> NULL</h2> 
+@endif 
+
+{{-- isset and empty --}}
+
+@isset($age)
+<h2>Age is {{$age}}</h2>
+@endisset
+
+@empty($age)
+    <h2>age is empty</h2>
+@endempty
+
+{{-- loop Foreach --}}
+@php
+$user = array("yahya","abdo","med")    
+@endphp
+
+@foreach ($user  as $value)
+    <p>User name id : {{ $value }}</p>
+@endforeach
+ 
